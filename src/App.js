279c18e743a8224504a38f667248bdfac719c68c
casey-minzi <cluvhope@gmail.com>
2020-03-03 14:30:21 +0000

    Added 6 TV show titles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
-import moviePoster from './a_lot_like_love_poster.jpg';
+import aLotLikeLove from './a_lot_like_love_poster.jpg';
+import prideAndPrejudice from './pride_and_prejudice_poster.jpg';
+import aboutTime from './about_time_poster.jpg';
+import whisperOfTheHeart from './whisper_of_the_heart_poster.jpg';
+import whenHarryMetSally from './when_harry_met_sally_poster.jpg';
+import parasite from './parasite_poster.jpg';
+
 
 function App() {
   return (
-    <div className ='App'>
-      <h1 className = 'movie_title'> A Lot Like Love</h1>
-      <div>
-        <img src = {moviePoster} alt = 'A Lot Like Love' />
+    <div className = 'App'>
+      <div className = 'flex_container'>
+        <h1 className = 'movie_title'> A Lot Like Love</h1>
+        <h1 className = 'movie_title'> Pride and Prejudice</h1>
+        <h1 className = 'movie_title'> About Time</h1>
+        <h1 className = 'movie_title'> Whisper of The Heart</h1>
+        <h1 className = 'movie_title'> When Harry Met Sally</h1>
+        <h1 className = 'movie_title'> Parasite</h1>
       </div>
     </div>
 
