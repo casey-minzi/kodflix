6e2a43b65b858a2be2b3aebec192087c270175ba
casey-minzi <cluvhope@gmail.com>
2020-03-04 15:04:37 +0000

    #6 animate the TV show title overlay

--- src/App.js
+++ src/App.js
@@ -14,19 +14,19 @@ function App() {
       <div className='container'>
         <div className='movie'>
           <img src={aLotLikeLove} className='item' alt='A Lot Like Love' />
-          <div className='title'>
+          <div className='title_overlay'>
             <h2>A Lot Like Love</h2>
           </div>
         </div>
         <div className='movie'>
           <img src={prideAndPrejudice} className='item' alt='Pride and Prejudice' />
-          <div className='title'>
+          <div className='title_overlay'>
             <h2>Pride and Prejudice</h2>
           </div>
         </div>
         <div className='movie'>
           <img src={aboutTime} className='item' alt='About Time' />
-          <div className='title'>
+          <div className='title_overlay'>
             <h2>About Time</h2>
           </div>
         </div>
@@ -34,19 +34,19 @@ function App() {
       <div className='container'>
         <div className='movie'>
           <img src={whisperOfTheHeart} className='item' alt='Whisper of the Heart' />
-          <div className='title'>
+          <div className='title_overlay'>
             <h2>Whisper of the Heart</h2>
           </div>
         </div>
         <div className='movie'>
           <img src={whenHarryMetSally} className='item' alt='When Harry Met Sally' />
-          <div className='title'>
+          <div className='title_overlay'>
             <h2>When Harry Met Sally</h2>
           </div>
         </div>
         <div className='movie'>
           <img src={parasite} className='item' alt='Parasite' />
-          <div className='title'>
+          <div className='title_overlay'>
             <h2>Parasite</h2>
           </div>
         </div>
