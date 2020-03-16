4e60f2deeea61e970c82eadad12219d6cec3df27
casey-minzi <cluvhope@gmail.com>
2020-03-16 13:08:24 +0000

    #13 Created Details component state

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-export default function Details() {
-	return (
-		<div>
-			<h2>
-				Hello, this will be the details page for each Movie & TV show :)
-      </h2>
-		</div>
-	)
+export default class Details extends Component {
+	constructor() {
+		super();
+		this.state = {
+			message: 'Hello, this will be the details page for each Movie & TV show :)'
+		}
+	}
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				message: 'Comming Soon!'
+			})
+		}, 3000)
+	}
+	render() {
+		return (
+		<div>{this.state.message}</div>
+		)
+	}
 }
