d0160aaa489076bdde2f0e836287762277285341
casey-minzi <cluvhope@gmail.com>
2020-03-19 14:55:07 +0000

    Added logo for testing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Details from './Details/Details';
 import NotFound from './NotFound/NotFound';
+import kodflixLogo from './kodflix_logo.png';
 
 function App() {
 	return (
 		<div className="App">
 			<header>
-				<h1> This is Kodflix</h1>
+				<h1>
+					<img src={kodflixLogo} alt="Kodflix logo" className="KodflixLogo"/>
+				</h1>
 			</header>
 			<Router>
 				<Switch>
