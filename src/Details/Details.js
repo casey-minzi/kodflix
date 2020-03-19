f2fd718896bbd05243e2f3ad7d9d584b4c1c47b2
casey-minzi <cluvhope@gmail.com>
2020-03-19 13:39:58 +0000

    # 16 Add synopsis & cover to each show

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gallery_get from '../Gallery/gallery_get';
 import { Redirect } from 'react-router-dom';
+import './Details.css';
 
 
 export default class Details extends Component {
@@ -21,7 +22,22 @@ export default class Details extends Component {
 		if (this.state.movie === undefined) {
 			return <Redirect to="/not-found" />;
 		} else {
-			return <h2>{this.state.movie.title}</h2>
-		}
+			return (
+			<div className="Details">
+				<div className="container">
+					<div className="item">
+						<h2 className="movieTitle">
+							{this.state.movie.title}
+						</h2>
+						<div className="synopsis">{this.state.movie.synopsis}</div>
+					</div>
+					<div className="item">
+						<div className="imageBox">
+							<img src={this.state.movie.image} alt={this.state.movie.title}/>
+						</div>
+					</div>
+				</div>
+			</div>
+		)}
 	}
 }
