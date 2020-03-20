cc3e49d0eaa49cc9f7dd6b7bb96f56f83e9405fb
casey-minzi <cluvhope@gmail.com>
2020-03-20 14:52:21 +0000

    #19-2 Refactored movieData var to function

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const movieData = require('./movieData');
+const { movieData } = require('./movieData');
 
-app.get('/rest/shows', (req, res) => res.send(movieData.map(movie => movie)));
+app.get('/rest/shows', (req, res) => res.send(movieData()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/backend/movieData.js
+++ src/backend/movieData.js
@@ -1,52 +1,54 @@
-const movieData = [{
-  title: 'A Lot Like Love', id: 'a_lot_like_love',
-  synopsis: `When Oliver (Ashton Kutcher) and Emily (Amanda Peet) meet on 
+function movieData() {
+  return [{
+    title: 'A Lot Like Love', id: 'a_lot_like_love',
+    synopsis: `When Oliver (Ashton Kutcher) and Emily (Amanda Peet) meet on 
   an airplane, they feel an instant connection, but agree that they're 
   wrong for each other. As the years pass and they live their separate 
   lives, fate brings them together several times, and they become close 
   friends. Any time one of them wants more out of the relationship, the 
   other seems content with just being friends. If they wait too long, 
   Oliver and Emily may miss any chance of spending their lives together.` },
-{
-  title: 'Pride and Prejudice', id: 'pride_and_prejudice',
-  synopsis: `In this adaptation of Jane Austen's beloved novel, Elizabeth 
+  {
+    title: 'Pride and Prejudice', id: 'pride_and_prejudice',
+    synopsis: `In this adaptation of Jane Austen's beloved novel, Elizabeth 
   Bennet (Keira Knightley) lives with her mother, father and sisters in 
   the English countryside. As the eldest, she faces mounting pressure from 
   her parents to marry. When the outspoken Elizabeth is introduced to the 
   handsome and upper-class Mr. Darcy (Matthew MacFadyen), sparks fly. 
   Although there is obvious chemistry between the two, Darcy's overly 
   reserved nature threatens the fledgling relationship.` },
-{
-  title: 'About Time', id: 'about_time',
-  synopsis: `When Tim Lake (Domhnall Gleeson) is 21, his father (Bill Nighy)
+  {
+    title: 'About Time', id: 'about_time',
+    synopsis: `When Tim Lake (Domhnall Gleeson) is 21, his father (Bill Nighy)
   tells him a secret: The men in their family can travel through time.
   Although he can't change history, Tim resolves to improve his life by 
   getting a girlfriend. He meets Mary (Rachel McAdams), falls in love and 
   finally wins her heart via time-travel and a little cunning. However, as 
   his unusual life progresses, Tim finds that his special ability can't 
   shield him and those he loves from the problems of ordinary life.` },
-{
-  title: 'Whisper of the Heart', id: 'whisper_of_the_heart',
-  synopsis: `Based on the manga with the same title, this animated film 
+  {
+    title: 'Whisper of the Heart', id: 'whisper_of_the_heart',
+    synopsis: `Based on the manga with the same title, this animated film 
   follows Shizuku, an inquisitive young girl and a voracious reader, who 
   longs to be a writer when she grows up. One day she notices that all of 
   her library books have previously been taken out by one Seiji Amasawa. 
   Amid chasing after a large cat, befriending an eccentric antiques dealer 
   and writing her first novel, Shizuku aims to find this mysterious boy 
   who may well be her soul mate.` },
-{
-  title: 'When Harry Met Sally', id: 'when_harry_met_sally',
-  synopsis: `In 1977, college graduates Harry Burns (Billy Crystal) and Sally 
+  {
+    title: 'When Harry Met Sally', id: 'when_harry_met_sally',
+    synopsis: `In 1977, college graduates Harry Burns (Billy Crystal) and Sally 
   Albright (Meg Ryan) share a contentious car ride from Chicago to New York, 
   during which they argue about whether men and women can ever truly be 
   strictly platonic friends. Ten years later, Harry and Sally meet again 
   at a bookstore, and in the company of their respective best friends, 
   Jess (Bruno Kirby) and Marie (Carrie Fisher), attempt to stay friends 
   without sex becoming an issue between them.` },
-{
-  title: 'Parasite', id: 'parasite',
-  synopsis: `Greed and class discrimination threaten the newly formed 
+  {
+    title: 'Parasite', id: 'parasite',
+    synopsis: `Greed and class discrimination threaten the newly formed 
   symbiotic relationship between the wealthy Park family and the destitute 
   Kim clan.` },]
+}
 
-  module.exports = movieData;
+module.exports.movieData = movieData
