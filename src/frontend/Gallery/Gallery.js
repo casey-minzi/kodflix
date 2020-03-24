0816f821d75e4d8f9684ea43f7433eaba04a5be8
casey-minzi <cluvhope@gmail.com>
2020-03-24 15:17:44 +0000

    #22 Connected the frontend & backend servers

--- src/frontend/Gallery/Gallery.js
+++ src/frontend/Gallery/Gallery.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Movie from '../Movie/Movie';
 import gallery_get from './gallery_get'
 
-export default function Gallery() {
-	return (
-		<div className="Movie">
-			<div className="container">
-				{gallery_get().map(({title, image, id}) => (
-					<Movie
-						key = {id}
-						title={title}
-						image={image}
-						id={id}
-					/>
-				))
-				}
+export default class Gallery extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			movie: {}
+		}
+	}
+
+	componentDidMount() {
+		fetch('/rest/shows')
+			.then(response => response.json())
+			.then(movie => {
+				console.log(movie)
+			})
+	}
+
+	render() {
+		return (
+			<div className="Movie">
+				<div className="container">
+					{gallery_get().map(({ title, image, id }) => (
+						<Movie
+							key={id}
+							title={title}
+							image={image}
+							id={id}
+						/>
+					))
+					}
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
