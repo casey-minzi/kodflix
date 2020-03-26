888a0d6bcd6cae5be47bab312f6e98ab2de418fc
casey-minzi <cluvhope@gmail.com>
2020-03-26 13:36:41 +0000

    fixing path error

--- src/backend/app.js
+++ src/backend/app.js
@@ -6,10 +6,10 @@ const { movieData } = require('./movieData');
 
 app.get('/rest/shows', (req, res) => res.send(movieData()));
 
-app.use(express.static(path.join(__dirname, '../../build')));
+app.use(express.static(path.join(__dirname, './../../build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../build', index.html));
+  res.sendFile(path.join(__dirname, './../../build', index.html));
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
