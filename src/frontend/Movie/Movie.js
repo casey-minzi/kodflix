f03a8f3e111bd5e76b0c15307388375d8adaa18c
casey-minzi <cluvhope@gmail.com>
2020-03-30 13:29:51 +0100

    indentation fix

--- src/frontend/Movie/Movie.js
+++ src/frontend/Movie/Movie.js
@@ -3,12 +3,18 @@ import './Movie.css';
 import { Link } from 'react-router-dom';
 
 
-export default function Movie({ image, title, id }) {
+export default function Movie({ title, id }) {
   return (
     <div className="item">
       <Link to={`/${id}`}>
         <div className="imageContainer">
-          <img src={require(`../Gallery/images/${id}.jpg`)} className="titleImg" alt={title} id={id} key={id}/>
+          <img 
+            src={require(`../Gallery/images/${id}.jpg`)}
+            className="titleImg"
+            alt={title}
+            id={id}
+            key={id}
+          />
         </div>
         <div className="titleOverlay">
           <h2>{title}</h2>
