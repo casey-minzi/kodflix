d199a0f8e5b44b58a3077d53f7ea91ada96cc4c2
casey-minzi <cluvhope@gmail.com>
2020-06-03 14:21:40 +0100

    #25 Added Loading indicator to details page

--- src/frontend/Details/Details.js
+++ src/frontend/Details/Details.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import './Details.css';
-import DetailsContent from './DetailsContent';
-
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import "./Details.css";
+import DetailsContent from "./DetailsContent";
+import Loading from "../common/Loading/Loading.js";
 
 export default class Details extends Component {
-	constructor() {
-		super();
-		this.state = {
-			movie: {}
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+    };
+  }
 
-	componentDidMount() {
-		let showId = this.props.match.params.showId;
-		let movie;
-		fetch('/rest/shows')
-			.then(response => response.json())
-			.then(movies => {
-				movie = movies.find(movie => movie.id === showId);
-				this.setState({ movie });
-			})
-	}
+  componentDidMount() {
+    let showId = this.props.match.params.showId;
+    let movie;
+    fetch("/rest/shows")
+      .then((response) => response.json())
+      .then((movies) => {
+        movie = movies.find((movie) => movie.id === showId);
+        this.setState({ movie });
+      });
+  }
 
-	render() {
-		let { movie } = this.state;
-		if (movie) {
-			return movie.id ?
-				<DetailsContent {...movie} /> :
-				<div />
-		} else {
-			return <Redirect to="/not-found" />;
-		}
-	}
+  render() {
+    let { movie } = this.state;
+    if (movie) {
+      return movie.id ? <DetailsContent {...movie} /> : <Loading />;
+    } else {
+      return <Redirect to="/not-found" />;
+    }
+  }
 }
--- src/frontend/NotFound/NotFound.js
+++ src/frontend/NotFound/NotFound.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import "./NotFound.css";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="NotFound">
       <h2> Ooops, it looks like this page doesn't exist... :( </h2>
     </div>
-  )
+  );
 }
